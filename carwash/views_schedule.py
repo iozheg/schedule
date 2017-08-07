bb73cf5636081df375067b7c65417623af36f361
alex <mozg@eml.ru>
2017-08-07 11:04:08 -0700

    added create and details views

---
+++ carwash/views_schedule.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models.schedule import ScheduleCreateForm, ScheduleDetailsForm, Schedule
+
+def create_schedule(request):
+    
+    if request.method == 'POST':
+        
+        form = ScheduleCreateForm(request.POST)
+        
+        if form.is_valid():
+            
+            form.save(request.user)
+            
+            return HttpResponseRedirect(reverse('owner-profile'))
+            
+    form = ScheduleCreateForm()
+    
+    return render(request, 'create_schedule.html', {'form': form})
+
+
+def schedule_details(request, schedule_id):
+    
+    if request.method == 'POST':
+        
+        form = ScheduleDetailsForm(request.POST)
+        
+        if form.is_valid():
+            
+            form.save(schedule_id)
+            
+            return HttpResponseRedirect(reverse('schedule-details', args=(schedule_id,)))
+            
+    else:
+        
+        schedule = get_object_or_404(Schedule, pk=schedule_id)
+        
+        initial_data = {
+            
+        }
+        
+        form = ScheduleDetailsForm(initial=schedule.__dict__)
+        
+    return render(request, 'schedule_details.html', {'form': form, 'id': schedule_id})
