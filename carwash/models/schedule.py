0ae9de002b0581a0894183661b093264e23df627
alex <mozg@eml.ru>
2017-08-07 11:01:55 -0700

    added create and details forms

--- carwash/models/schedule.py
+++ carwash/models/schedule.py
@@ -1,5 +1,7 @@
 import datetime
 from django.db import models
+from django import forms
+
 from .user import BusinessProfile
 
 class Schedule(models.Model):
@@ -19,7 +21,7 @@ class Schedule(models.Model):
     get total working days.
     
     work_time_start, work_time_end - defines working hours of car wash.
-    If work_time_start == work_time_end than around the clock.
+    If work_time_start == work_time_end or blank than around the clock.
     
     dinner_break_start, dinner_break_ens - defines dinner time. 
     If one of them is blank, then no dinner.
@@ -34,7 +36,7 @@ class Schedule(models.Model):
     
     """
     
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     description = models.TextField(max_length=300)
     tel_number = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
@@ -46,10 +48,55 @@ class Schedule(models.Model):
     dinner_break_start = models.TimeField(blank=True)
     dinner_break_end = models.TimeField(blank=True)
     
-    time_interval = models.TimeField(datetime.time(0, 15))
+    time_interval = models.TimeField(default=datetime.time(0, 15))
     checkin_amount = models.SmallIntegerField(default=1)
     
     active = models.BooleanField(default=True)
     create_date = models.DateField(auto_now_add=True)
     
     owner = models.ForeignKey(BusinessProfile, null=True, on_delete=models.SET_NULL)
+
+class ScheduleCreateForm(forms.Form):
+    
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=300, widget=forms.Textarea)
+    tel_number = forms.CharField(label='Telephone', max_length=30)
+    address = forms.CharField(max_length=100)
+    
+    work_days = forms.IntegerField(initial=128, widget=forms.NumberInput)
+    work_time_start = forms.TimeField(required=False)
+    work_time_end = forms.TimeField(required=False)
+    
+    dinner_break_start = forms.TimeField(required=False)
+    dinner_break_end = forms.TimeField(required=False)
+    
+    time_interval = forms.TimeField(initial=datetime.time(0, 15))
+    checkin_amount = forms.IntegerField(initial=1, widget=forms.NumberInput)
+    
+    active = forms.BooleanField(initial=True)
+
+    def save(self, user):
+        self.cleaned_data['owner'] = user
+        new_schedule = Schedule.objects.create(**self.cleaned_data)
+
+class ScheduleDetailsForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=300, widget=forms.Textarea)
+    tel_number = forms.CharField(label='Telephone', max_length=30)
+    address = forms.CharField(max_length=100)
+    
+    work_days = forms.IntegerField(initial=128, widget=forms.NumberInput)
+    work_time_start = forms.TimeField(required=False)
+    work_time_end = forms.TimeField(required=False)
+    
+    dinner_break_start = forms.TimeField(required=False)
+    dinner_break_end = forms.TimeField(required=False)
+    
+    time_interval = forms.TimeField(initial=datetime.time(0, 15))
+    checkin_amount = forms.IntegerField(initial=1, widget=forms.NumberInput)
+    
+    active = forms.BooleanField(initial=True)
+
+    def save(self, schedule_id):
+     #   self.cleaned_data['id'] = schedule_id
+        schedule = Schedule.objects.update_or_create(pk=schedule_id, defaults=self.cleaned_data)
