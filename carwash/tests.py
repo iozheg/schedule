aee687baf58cc1b9c587bc2bf08b4d79d18bac03
alex <mozg@eml.ru>
2017-08-10 12:40:43 -0700

    added tests for checkins and schedules

--- carwash/tests.py
+++ carwash/tests.py
@@ -2,7 +2,7 @@ import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from .models import ClientProfile, BusinessProfile, Schedule, Checkin
+from .models import ClientProfile, BusinessProfile
 
 class ClientProfileTests(TestCase):
     
@@ -39,92 +39,3 @@ class BusinessProfileTests(TestCase):
         
         self.assertIs(self.user==User.objects.get(username='owner@test.com'), True)
         self.assertIs(BusinessProfile.objects.get(user=self.user).tel_number=='8(919)1991', True)
-
-
-class ScheduleAndCheckinTests(TestCase):
-    
-    def setUp(self):
-        
-        self.user = User.objects.create_user(
-            username='owner@test.com', password='123456789'
-        )
-        self.profile = BusinessProfile.objects.create(
-            user=self.user, name='owneruser', tel_number='8(919)1991'
-        )        
-        
-        self.user2 = User.objects.create_user(
-            username='test@test.com', password='123456789'
-        )
-        
-        self.profile = ClientProfile.objects.create(
-            user=self.user2, name='testuser', tel_number='8(919)1991234', 
-            car_model='ford mondeo 3', car_reg_number='a604 HK 89'
-        )
-        
-        initial_data_1 = {
-            'work_time_end': datetime.time(22, 0), 
-            'checkin_amount': 1, 
-            'dinner_break_end': datetime.time(13, 0), 
-            'work_time_start': datetime.time(8, 0), 
-            'work_days': 128, 
-            'name': 'first', 
-            'time_interval': datetime.time(0, 15), 
-            'id': 1, 
-            'description': 'this is first', 
-            'tel_number': '89198181111', 
-            'dinner_break_start': datetime.time(12, 0), 
-            'address': 'Tyumen, respubliki 169', 
-            'active': True, 
-            'owner': BusinessProfile.objects.get(user=self.user)
-        }
-
-        self.sch = Schedule.objects.create(**initial_data_1)
-        
-        Schedule.objects.create(**{
-            'work_time_end': datetime.time(0, 0), 'checkin_amount': 1, 
-            'dinner_break_end': None, 'work_time_start': datetime.time(0, 0), 
-            'work_days': 128, 'name': 'second', 
-            'time_interval': datetime.time(0, 15), 'id': 2, 
-            'description': 'second one', 'tel_number': '5jg9875', 
-            'dinner_break_start': None, 'address': 'hg374g73h4987', 
-            'active': True, 'owner': BusinessProfile.objects.get(user=self.user)
-        })
-        
-        self.ch = Checkin.objects.create(
-            date=datetime.date(2017,8,9),
-            time=datetime.time(8,0),
-            schedule=self.sch,
-            client=self.user2.profile
-        )
-    
-    def test_Schedule_was_created(self):
-                 
-        self.assertIs(Schedule.objects.get(pk=1).name == 'first',True)
-        self.assertIs(Schedule.objects.get(pk=1).work_time_start == datetime.time(8,0,0),True)
-        self.assertIs(Schedule.objects.get(pk=2).owner == BusinessProfile.objects.get(user=self.user),True)
-        self.assertIs(Schedule.objects.get(pk=2).address == 'hg374g73h4987',True)
-        
-    def test_Schedule_DoesNotExist(self):
-        
-        with self.assertRaisesMessage(Schedule.DoesNotExist, ''):
-            Schedule.objects.get(pk=5)
-
-    def test_Checkin_was_created(self):
-        
-        self.assertIs(Checkin.objects.get(client=self.user2.profile) == self.ch, True)
-        
-    def test_Checkin_DoesNotExist(self):
-        
-        with self.assertRaisesMessage(Checkin.DoesNotExist, ''):
-            Checkin.objects.get(pk=5)
-            
-        with self.assertRaisesMessage(Checkin.DoesNotExist, ''):
-            Checkin.objects.get(schedule=Schedule.objects.get(pk=2))
-            
-    def test_Checkin_Schedule_relation(self):
-        
-        self.assertIs(Checkin.objects.get(client=self.user2.profile).schedule==self.sch, True)
-        
-        with self.assertRaisesMessage(Schedule.DoesNotExist, ''):
-            Checkin.objects.get(schedule=Schedule.objects.get(pk=5))
-
---
+++ carwash/tests_checkin_schedule.py
@@ -0,0 +1,189 @@
+import datetime
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from .models import ClientProfile, BusinessProfile, Schedule, Checkin
+
+class ScheduleAndCheckinTests(TestCase):
+    
+    def setUp(self):
+        
+        self.user = User.objects.create_user(
+            username='owner@test.com', password='123456789'
+        )
+        self.profile = BusinessProfile.objects.create(
+            user=self.user, name='owneruser', tel_number='8(919)1991'
+        )        
+        
+        self.user2 = User.objects.create_user(
+            username='test@test.com', password='123456789'
+        )
+        
+        self.profile = ClientProfile.objects.create(
+            user=self.user2, name='testuser', tel_number='8(919)1991234', 
+            car_model='ford mondeo 3', car_reg_number='a604 HK 89'
+        )
+        
+        initial_data_1 = {
+            'name': 'first', 
+            'description': 'this is first', 
+            'tel_number': '89198181111', 
+            'address': 'Tyumen, respubliki 169',
+            'work_days': 128,   
+            'work_time_start': datetime.time(8, 0), 
+            'work_time_end': datetime.time(22, 0),              
+            'dinner_break_start': datetime.time(12, 0), 
+            'dinner_break_end': datetime.time(13, 0),           
+            'time_interval': datetime.time(0, 10), 
+            'checkin_amount': 1,  
+            'active': True, 
+            'owner': BusinessProfile.objects.get(user=self.user)
+        }
+
+        self.sch1 = Schedule.objects.create(**initial_data_1)
+        
+        self.sch2 = Schedule.objects.create(**{
+            'name': 'second', 'description': 'second one', 
+            'tel_number': '(919)5jg9875', 'address': 'hg374g73h4987', 
+            'work_days': 128, 'work_time_start': datetime.time(0, 0), 
+            'work_time_end': datetime.time(0, 0), 
+            'dinner_break_start': None, 'dinner_break_end': None, 
+            'checkin_amount': 1, 'time_interval': datetime.time(0, 15),
+            'active': True, 
+            'owner': BusinessProfile.objects.get(user=self.user)
+        })
+        
+        self.sch3 = Schedule.objects.create(**{
+            'name': 'third', 'description': 'third one', 
+            'tel_number': '34-58-77', 'address': 'Moscow', 
+            'work_days': 63, 'work_time_start': datetime.time(0, 0), 
+            'work_time_end': datetime.time(0, 0), 
+            'dinner_break_start': datetime.time(12, 0), 
+            'dinner_break_end': datetime.time(14, 0), 
+            'checkin_amount': 2, 'time_interval': datetime.time(0, 30),
+            'active': True, 
+            'owner': BusinessProfile.objects.get(user=self.user)
+        })
+        
+        self.ch = Checkin.objects.create(
+            date=datetime.date(2017,8,9), time=datetime.time(8,0),
+            schedule=self.sch1, client=self.user2.profile
+        )        
+    
+    def test_Schedule_was_created(self):
+        
+        self.assertIs(Schedule.objects.get(name='first').description == 'this is first',True)
+        self.assertIs(Schedule.objects.get(name='first').work_time_start == datetime.time(8,0,0),True)
+        self.assertIs(Schedule.objects.get(name='second').owner == BusinessProfile.objects.get(user=self.user),True)
+        self.assertIs(Schedule.objects.get(name='second').address == 'hg374g73h4987',True)
+        
+    def test_Schedule_DoesNotExist(self):
+        
+        with self.assertRaisesMessage(Schedule.DoesNotExist, ''):
+            Schedule.objects.get(pk=5)
+
+    def test_Checkin_was_created(self):
+        
+        self.assertIs(Checkin.objects.get(client=self.user2.profile) == self.ch, True)
+        
+    def test_Checkin_DoesNotExist(self):
+        
+        with self.assertRaisesMessage(Checkin.DoesNotExist, ''):
+            Checkin.objects.get(pk=500)
+            
+        #with self.assertRaisesMessage(Checkin.DoesNotExist, ''):
+        #    Checkin.objects.get(schedule=Schedule.objects.get(pk=100))
+            
+    def test_Checkin_Schedule_relation(self):
+        
+        self.assertIs(Checkin.objects.get(client=self.user2.profile).schedule==self.sch1, True)
+        
+        with self.assertRaisesMessage(Schedule.DoesNotExist, ''):
+            Checkin.objects.get(schedule=Schedule.objects.get(pk=50))
+            
+    def test_checkin_amount(self):
+        
+        for i in range(3):
+            Checkin.objects.create(
+                date=datetime.date(2017,8,10), time=datetime.time(8,35),
+                schedule=self.sch1, client=self.user2.profile
+            )
+        
+        for i in range(2):
+            Checkin.objects.create(
+                date=datetime.date(2017,8,11), time=datetime.time(8,27),
+                schedule=self.sch2, client=self.user2.profile
+            )
+        
+        Checkin.objects.create(
+            date=datetime.date(2017,8,11), time=datetime.time(10,15),
+            schedule=self.sch2, client=self.user2.profile
+        )
+        
+        Checkin.objects.create(
+            date=datetime.date(2017,8,11), time=datetime.time(10,15),
+            schedule=self.sch3, client=self.user2.profile
+        )
+        
+        #sch1
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch1.id,
+                datetime.date(2017,8,10),
+                datetime.time(8,35)
+            ),
+            3
+        )        
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch1.id,
+                datetime.date(2017,9,10),
+                datetime.time(8,35)
+            ),
+            0
+        )
+        
+        #sch2
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch2.id,
+                datetime.date(2017,8,11),
+                datetime.time(8,27)
+            ),
+            2
+        )       
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch2.id,
+                datetime.date(2017,9,10),
+                datetime.time(8,27)
+            ),
+            0
+        ) 
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch2.id,
+                datetime.date(2017,8,11),
+                datetime.time(10,15)
+            ),
+            1
+        )
+        
+        #sch3        
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch3.id,
+                datetime.date(2017,8,11),
+                datetime.time(10,15)
+            ),
+            1
+        )       
+        self.assertIs(
+            Checkin.objects.get_checkin_amount_by_datetime(
+                self.sch3.id,
+                datetime.date(2017,9,10),
+                datetime.time(8,27)
+            ),
+            0
+        ) 
+
