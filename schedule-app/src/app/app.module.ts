67a7e8dd9071cda2a50603fa632e014c4e6f0999
alex <mozg@eml.ru>
2017-09-26 14:03:43 -0700

    initial app

--- schedule-app/src/app/app.module.ts
+++ schedule-app/src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdToolbarModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdMenuModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdDatepickerModule } from '@angular/material';
+import { MdNativeDateModule } from '@angular/material';
+import { MdCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { CarwashSearchComponent } from './checkin/carwash-search.component';
+import { ScheduleChoiceComponent } from './checkin/schedule-choice.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent,
+    CarwashSearchComponent,
+    ScheduleChoiceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdCardModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
