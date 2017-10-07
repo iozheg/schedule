66f3e2aaa51b43c4117c0727aebaf222aba36a55
alex <mozg@eml.ru>
2017-10-08 00:48:03 +0500

    added autocomplete in carwash name input feature

--- schedule-app/src/app/checkin/carwash-search/carwash-search.component.ts
+++ schedule-app/src/app/checkin/carwash-search/carwash-search.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit }    from '@angular/core';
+import { Router }               from '@angular/router';
+//import { HttpClient } from '@angular/common/http';
+
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
+ 
+// Observable class extensions
+import 'rxjs/add/observable/of';
+ 
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
 
 import { SearchParamService } from '../search-param.service';
-
+import { ScheduleService } from '../../schedule.service';
 
 @Component({
-    selector: 'carwash-search',
+//    selector: 'carwash-search',
     templateUrl: 'carwash-search.component.html',
     styleUrls: ['carwash-search.component.css']
 })
@@ -15,19 +28,38 @@ export class CarwashSearchComponent implements OnInit {
 
     carwashName: string;
     datepickerDate: Date;
-//    date: Date;
+    options: string[];
+
+    schedulesNames: Observable<string[]>;
+    private searchTerms = new Subject<string>();
 
     constructor( 
         private searchParam: SearchParamService,
         private router: Router,
-        private http: HttpClient
+        private scheduleService: ScheduleService
     ) { }
+
+    searchSchedulesByName(term: string): void{
+        this.searchTerms.next(term);
+    }
 
     ngOnInit() { 
         this.carwashName = this.searchParam.carwashName;
         this.datepickerDate = this.searchParam.date || new Date();
 
-        this.http.get('127.0.0.1')
+    //    this.options = ['first', 'second', 'sixth'];
+
+        this.schedulesNames = this.searchTerms
+            .debounceTime(300)
+            .distinctUntilChanged()
+            .switchMap( term => term 
+                ? this.scheduleService.searchSchedulesByName(term)
+                : Observable.of<string[]>([]))
+            .catch(error => {
+                // TODO: add real error handling
+                console.log(error);
+                return Observable.of<string[]>([]);
+            });
     }
 
     onChange(event: any): void{
@@ -44,4 +76,9 @@ export class CarwashSearchComponent implements OnInit {
 
         this.router.navigate(['/schedules']);
     }
+
+    filter(val: string): string[] {
+        return this.options.filter(option =>
+          option.toLowerCase().indexOf(val.toLowerCase()) === 0);
+     }
 }
