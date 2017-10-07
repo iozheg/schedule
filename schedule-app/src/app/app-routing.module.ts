37cc0d8d791b4aff9c77200b2d1823fc03082855
alex <mozg@eml.ru>
2017-10-07 13:00:45 +0500

    added route to FinishCheckinComponent

--- schedule-app/src/app/app-routing.module.ts
+++ schedule-app/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { CarwashSearchComponent } from './checkin/carwash-search/carwash-search.component';
 import { ScheduleChoiceComponent } from './checkin/schedule-choice/schedule-choice.component';
 import { TimeChoiceComponent } from './checkin/time-choice/time-choice.component';
+import { FinishCheckinComponent } from './checkin/finish-checkin/finish-checkin.component';
 
 const routes: Routes = [
   { path: 'schedules/time', component: TimeChoiceComponent},
+  { path: 'schedules/checkin', component: FinishCheckinComponent},
   { path: 'schedules', component: ScheduleChoiceComponent},
   { path: '', component: CarwashSearchComponent },
 ];
