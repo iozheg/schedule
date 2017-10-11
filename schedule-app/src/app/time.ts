e9e90116ee1282d68ba0423a4ff501b6f5a68981
alex <mozg@eml.ru>
2017-10-12 00:40:08 +0500

    added classes for work with dates, date ranges

--- schedule-app/src/app/time.ts
+++ schedule-app/src/app/time.ts
@@ -62,8 +62,10 @@ export class TimeRangeClass {
     private startTime: TimeClass;
     private endTime: TimeClass;
     private duration: number;
+    private dinnerTimeStart: TimeClass;
+    private dinnerTimeEnd: TimeClass;
 
-    constructor(timeRangeString: string){
+    constructor(timeRangeString: string, dinnerRangeString: string){
         /*
             Receives two time strings and parses it to two TimeClass properties.
             If the end of the time range is after midnight than afterMidnight is true.
@@ -72,6 +74,15 @@ export class TimeRangeClass {
         let timeArray = timeRangeString.split('-');
         this.startTime = new TimeClass(timeArray[0]);
         this.endTime = new TimeClass(timeArray[1]);
+
+        try{
+            let dinnerTimeArray = dinnerRangeString.split('-');
+            this.dinnerTimeStart = new TimeClass(dinnerTimeArray[0]);
+            this.dinnerTimeEnd = new TimeClass(dinnerTimeArray[1]);
+        }
+        catch(e){
+            this.dinnerTimeStart = this.dinnerTimeEnd = null;
+        }
            
         if(this.startTime.toMinutes() >= this.endTime.toMinutes()) {
             this.duration = this.endTime.toMinutes() + (1440 - this.startTime.toMinutes());
@@ -94,7 +105,15 @@ export class TimeRangeClass {
         let currentDuration = 0;
 
         while(true){
-            array.push(currentTime.toString());
+            // If schedule has dinner we exclude dinner time from array
+            if(this.dinnerTimeStart == null || this.dinnerTimeStart == null
+                || currentTime.lessThan(this.dinnerTimeStart) 
+                || currentTime.greaterThan(this.dinnerTimeEnd))
+                {
+                    array.push(currentTime.toString());
+                }
+
+            
             currentTime.add(interval);
             
             // Count current duration from the start of generation.
@@ -105,6 +124,155 @@ export class TimeRangeClass {
         }
         return array;
     }
+}
+
+export class ExtendedDateClass{
+    static dateSetTime(date: Date, time: string): Date {
+
+        let newDate = new Date(date);
+        
+        try{
+            let timeArray = time.split(":");
+            
+            newDate.setHours(+timeArray[0]);
+            newDate.setMinutes(+timeArray[1]);
+            newDate.setSeconds(+timeArray[2]);
+        }
+        catch(error){
+            // If string is not in format 'hh:mm:ss' than
+            // return null
+            newDate = null;
+        }
+    
+        return newDate;
+    }
+
+    static dateSetOnlyDate(oldDate: Date, newDate: Date): Date {
+
+        let date = new Date(oldDate);
+
+        date.setDate(newDate.getDate());
+        date.setMonth(newDate.getMonth());
+        date.setFullYear(newDate.getFullYear());
+
+        return date;
+    }
+
+    static dateAdd(date: Date, el: string, value: number): Date{
+        
+        let newDate = new Date(date);
+
+        switch (el){
+            case 'day':
+            newDate.setDate(date.getDate() + value);
+                break;
+            case 'minute':
+            newDate.setMinutes(date.getMinutes() + value);
+                break;
+            case 'hour':
+            newDate.setHours(date.getHours() + value);
+                break;
+        }
+        return newDate;
+    }
+
+    static equal(date1: Date, date2: Date): boolean {
+        return (date1.getHours() == date2.getHours()
+            && date1.getMinutes() == date2.getMinutes()
+            && date1.getDate() == date2.getDate()
+            && date1.getMonth() == date2.getMonth()
+            && date1.getFullYear() == date2.getFullYear()
+        )
+    }
+}
+
+export class DateTimeClass{
+    date: Date;
+    status: string;
+
+    constructor(date: Date){
+        this.date = new Date(date);
+        this.status = 'active';
+    }
+
+    get time(): string {
+        
+        let hours: string;
+        let minutes: string;
+        
+        hours = this.date.getHours() < 10 ? "0" + this.date.getHours() : this.date.getHours().toString();
+        minutes = this.date.getMinutes() < 10 ? "0" + this.date.getMinutes() : this.date.getMinutes().toString();
+
+        return hours + ":" + minutes;
+    }
+
+    get value(): string{
+        return this.date.getFullYear() + '-'
+            + this.date.getMonth() + '-'
+            + this.date.getDate() + 'T'
+            + this.date.getHours() + ':'
+            + this.date.getMinutes() + ':'
+            + this.date.getSeconds();
+    }
+}
+
+export class DateRangeClass{
+    rangeStart: DateTimeClass;
+    rangeEnd: DateTimeClass;
+    dateRange: DateTimeClass[];
+
+    constructor(start: Date, end: Date, interval: Date){
+
+    //    this.rangeStart = new Date(start);
+
+        let intervalInMinutes = interval.getHours() * 60 + interval.getMinutes();
+        let current = new Date(start);
+       
+        let timeArray: DateTimeClass[] = [];
+        let i = 0;
+        while(current < end && i < 200){
+            timeArray.push(new DateTimeClass(current));
+            current = ExtendedDateClass.dateAdd(current, 'minute', intervalInMinutes);
+            i++;
+        }
+
+        this.dateRange = timeArray;
+
+        this.rangeStart = timeArray[0];
+        this.rangeEnd = timeArray[timeArray.length-1];
+    }
+
+    excludeRange(range: DateRangeClass){
+        this.dateRange.forEach(elem =>{
+            if(range.isDateIncluded(elem.date))
+                elem.status = 'occupied';
+        });
+    }
+
+    excludeRangev2(start: Date, end: Date){
+        this.dateRange.forEach(elem =>{
+            if(elem.date >= start && elem.date < end)
+                elem.status = 'occupied';
+        });
+    }
+
+    excludeDate(date: Date){
+        
+        this.dateRange.forEach(elem => {
+        //    console.log('from list: ' + elem.date);
+        //    console.log('from serv: ' + date);
+            if(ExtendedDateClass.equal(elem.date, date)){
+            //    console.log('hit');
+                elem.status = 'occupied';
+                return;
+            }
+        });
+    }
+
+    private isDateIncluded(date: Date){
+        return (this.rangeStart.date < date && this.rangeEnd.date > date);
+    }
+}
 /*
 	EndTimeInMinutes = function(){
 		return this.startTime.toMinutes() + this.duration;
@@ -137,4 +305,3 @@ export class TimeRangeClass {
 	IsAroundTheClock = function(){
 		return this.startTime.getStringTime() == this.endTime.getStringTime();
 	}*/
-}
