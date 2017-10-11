cefefcbe2e5619fa56d1ec24a153061fa78e9060
alex <mozg@eml.ru>
2017-10-12 00:52:29 +0500

    Removed using mock schedules. Now component recieves selected date only from search-param component.

--- schedule-app/src/app/checkin/schedule-choice/schedule-choice.component.ts
+++ schedule-app/src/app/checkin/schedule-choice/schedule-choice.component.ts
@@ -6,7 +6,8 @@ import { Router } from '@angular/router';
 
 import { Schedule } from '../schedule';
 import { SearchParamService } from '../search-param.service';
-import { SCHEDULES, getScheduleById } from '../mock-schedules';
+import { SCHEDULES } from '../mock-schedules';
+import { ScheduleService } from '../../schedule.service';
 
 @Component({
     //selector: 'schedule-choice',
@@ -18,23 +19,37 @@ export class ScheduleChoiceComponent implements OnInit {
     
     carwashName: string;
     date: Date;
-    schedules = SCHEDULES;
+    schedules: Schedule[];
     
     constructor(
         private router: Router,
-        private searchParam: SearchParamService
+        private searchParam: SearchParamService,
+        private scheduleService: ScheduleService
     ) {}
 
     ngOnInit() { 
-        this.carwashName = this.searchParam.carwashName;
-        this.date = this.searchParam.date || new Date();
+        this.carwashName = this.searchParam.carwashName || '';
+        this.date = this.searchParam.date;
+
+        this.scheduleService.getSchedulesBriefInfo(this.carwashName)
+            .then( schedules => this.schedules = schedules );
+
+    //    console.log(this.searchParam);
     }
 
     scheduleClick(id): void{
         this.searchParam.date = this.date;
         this.searchParam.selectedScheduleId = id;
-        this.searchParam.carwashName = getScheduleById(id).name;
+    //    this.searchParam.carwashName = getScheduleById(id).name;
         
         this.router.navigate(['/schedules/time']);
     }
+/*
+    getScheduleById(id: number): Schedule{
+        for(let sch in this.schedules){
+            if (SCHEDULES[sch].id == id)
+                return SCHEDULES[sch];
+        }
+    
+        return null;*/
 }
